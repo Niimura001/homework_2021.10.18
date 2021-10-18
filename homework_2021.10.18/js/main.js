e9722df97812b43dbce3f6cf4b614435cf49d33a
Niimura001 <88579417+Niimura001@users.noreply.github.com>
2021-10-18 15:23:52 +0900

    Add files via upload

--- homework_2021.10.18/js/main.js
+++ homework_2021.10.18/js/main.js
@@ -1,10 +1,28 @@
-const array = [2, 4, 7, 5, 4, 3, 8];
-let set = new Set(array);
-const indexOfJS = array.indexOf(set);
-console.log(indexOfJS);
+let array = [2, 4, 7, 5, 4, 3, 8];
+let result = array.filter( function (value, index, array) {
+    // indexを比較し、重複データを排除
+    return array.indexOf( value ) === index;
+})
+// 結果出力
+console.log('result: '+ result);
+//https://www.fenet.jp/dotnet/column/language/7232/
 
-var data = [1, 4, 7, 12, 21];
-var result = data.filter(function(value) {
-  return value % 2 === 1;
-});
-console.log(result);
+//うるう年の判定
+function isLeapYear(year){
+  if( (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true;
+  }
+  return false;
+}
+let year = 2020;
+if (isLeapYear(year)) {
+  console.log(year + '年はうるう年です');
+} else {
+  console.log(year + '年はうるう年ではありません');
+}
+let check = 2021;
+if (isLeapYear(check)) {
+  console.log(check + '年はうるう年です');
+} else {
+  console.log(check + '年はうるう年ではありません');
+}
